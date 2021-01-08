306c90eab1596c9be715ce98dc269441ce008b36
Dexter <dex429.dc@gmail.com>
2021-01-08 18:23:12 -0500

    commit tree 23f61dec4b02d7267afb915b1114cdd1e03a274b

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+    #def initialize(name, breed)
+       # @name = name
+       # @breed = breed
+   # end
+    def name=(name)
+        @name = name
+    end
+    def job=(job)
+        @job = job
+    end
+    def name
+        @name
+    end
+    def job
+        @job
+    end
+
+end
